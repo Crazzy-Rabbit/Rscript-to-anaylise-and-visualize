adc77f796e558a917ad0cf8dc1879529827f218b
Lulu Shi <crazzy_rabbit@163.com>
2023-06-15 22:14:52 +0800

    Update plot_Hap_pheatmap.r

--- plot_Hap_pheatmap.r
+++ plot_Hap_pheatmap.r
@@ -1,7 +1,5 @@
 # 不同组之间有缝隙，更加直观
 library(pheatmap)
-library(ComplexHeatmap)
-
 group = read.table("sample_plot.list.txt", stringsAsFactors = F) ##两列，1：ID，2：品种
 names(group) <- c("ID","Group")
 rownames(group) <- group$ID
@@ -13,15 +11,16 @@ group$Group <- factor(group$Group, levels = new_order)
 data2=read.table ("LRP2BP.SNP.biallele.txt", header = TRUE, stringsAsFactors = F) # 转置后的文件
 data2=data.matrix(data2)
 
-rowcol = c("#873186", "#E20593",  "#6BB93F", "#18A2CA")# "#3364BC", "#000000", "#F37020", "#DBB71D"
-names(rowcol) <- unique(group$Group)
-ra_col <- list(Group=rowcol)
-my_annotation <- rowAnnotation(df=group,col=list(Group=rowcol),gp=gpar(fontsize=12)) # 添加注释条
+annotation_row = data.frame(Group=as.vector(group$Group))
+rownames(annotation_row) = rownames(data2)
+
+names(rowcol) <- unique(annotation_row)
+ra_col <- list(Group=c(JPBC="#873186", Mongolian="#E20593",  Kazakh="#6BB93F", Yanbian="#18A2CA"))
+# "#3364BC", "#000000", "#F37020", "#DBB71D"
 gap_line <- c(11,21,32) # 数量划分，分开每组的热图
 
-p = pheatmap(data2, cluster_row = FALSE,cluster_col = FALSE,
-             gaps_row=gap_line,legend =FALSE,
-             labels_col = rep("",ncol(data2)),labels_row=rep("",nrow(data2)),
-             color=c("#F7F8D5", "#BF3826"),angle_col = "0")
-
-p + my_annotation
+pheatmap(data2,annotation_row = annotation_row, annotation_colors = ra_col,
+        cluster_row = FALSE,cluster_col = FALSE,
+        gaps_row=gap_line,legend =FALSE,
+        labels_col = rep("",ncol(data2)),labels_row=rep("",nrow(data2)),
+        color=c("#F7F8D5", "#BF3826"), angle_col = "0")
