f37f15db454aa280c87a308ecbb89f1b1abd2c92
Lulu Shi <crazzy_rabbit@163.com>
2023-09-25 19:44:39 +0800

    Update plot_Manhantan.R

--- plot_Manhantan.R
+++ plot_Manhantan.R
@@ -1,4 +1,3 @@
-
 ##曼哈顿图
 # install.packages('qqman')
 
@@ -9,14 +8,13 @@ library('qqman')
 data1 <- read.table("FST.csv",header = T,sep = ',') 
 color_set <- c("#801e91","#344fa8","#f7cb34","#a8a8aa","#ffe4c7")
 
-par(cex=0.9,font=2)       # 调整整个图的大小比例
- par(cex.lab=1.5)   # 调整坐标轴标题大小
-par(cex.axis=1)    # 调整坐标轴刻度线大小
+par(cex=0.9,font=2)
+par(cex.lab=1.5)
+par(cex.axis=1)
 manhattan(data1,col=color_set, logp  = F,ylab='Fst',ylim=c(0,0.8),
           genomewideline=-log10(5e-800),suggestiveline = -log10(1e-800) )
-
 abline(lty = 1 , h =0.255368, col = "red",lwd = 3)
-#444444
+
 ##logp一般为F,若为T则y轴取值为-logp值  
 ##ylim=c(0,7) 可限制y轴的范围   
 # col   :  线条的颜色
@@ -45,6 +43,27 @@ CMplot(data1, plot.type="m", LOG10=F,
        ###ylab = expression(paste(italic('F'),st)),
        
        pch =".", cex =4, dpi = 600, file = "jpg",memo = "IHS")
+
+
+## circol 图
+#SNP chr    pos Somatic cell score  Milk yield Fat percentage
+#1 SNP1   1  59082        0.000244361 0.000484255    0.001379210
+#2 SNP2   1 118164        0.000532272 0.000039800    0.000598951
+data1 <- read.table("wag_A-H.windowed.weir.fst",sep='\t', header = TRUE)
+data2 <- read.table("A_H-Wag.norm.XPEHH",sep='\t', header = TRUE)
+data3 <- read.table("wag_A-H-lnratio.txt", sep='\t', header = TRUE)
+
+merged_data <- merge(merge(data1, data2, by=c("CHR","BP")), 
+                     data3, by=c("CHR","BP"))
+input_data <- merged_data[,c(3,1,2,4,6,8)]
+CMplot(input_data,type="p",plot.type="c",LOG10=FALSE,outward=TRUE,
+       col=matrix(c("#4DAF4A", NA, NA,
+                    "dodgerblue4","deepskyblue", NA,
+                    "dodgerblue1", "olivedrab3", "darkgoldenrod1"), 
+                    nrow=3, byrow=TRUE),
+       chr.labels=paste("Chr",c(1:29),sep=""),threshold=NULL,r=1.5,cir.chr.h=1.5,
+       cir.band=1,file="jpg", dpi=600,chr.den.col="black",
+       file.output=T,verbose=TRUE,width=10,height=10)
 
 
 ## 可视化在每个染色体上选择信号的值
