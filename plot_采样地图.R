eab65f684a5a6d3dd51ae31d0ee97949f2f3588d
Lulu Shi <crazzy_rabbit@163.com>
2024-01-12 17:14:58 +0800

    Update and rename 采样地图.R to plot_采样地图.R

--- plot_采样地图.R
+++ plot_采样地图.R
@@ -4,34 +4,6 @@ library(ggthemes)
 mymap <- read.table("经纬度.txt", sep = "\t", header =T)
 world <- map_data("world")
 
-my_col = c("Bos_indicus"="black","Bos_taurus"="black", "Bos_taurus_Bos_indicus"="black")
-my_fill = c("Bos_indicus"="#98F5FF","Bos_taurus"="#FF7F00", "Bos_taurus_Bos_indicus"="black")
-my_shape = c("Bos_indicus"=22,"Bos_taurus"=21, "Bos_taurus_Bos_indicus"=19)
-
-p1 <- ggplot(world, aes(long, lat)) +
-         geom_map(map=world, aes(map_id=region), fill="#DEDEDE", color=NA) +
-         xlim(-105, 135)+ ylim(-50, 60)+
-         coord_quickmap() 
-
-p2 <- p1 + geom_point(data=mymap, aes(x = Longitude, y = Latitude, 
-                      group=subspe,size=num, color=subspe, shape=subspe, fill=subspe))+
-           scale_fill_manual(values = my_fill)+
-           scale_color_manual(values = my_col)+
-           scale_shape_manual(values = my_shape)+
-           theme_map()+
-           theme(legend.position=c(0,0),legend.justification=c(0,0),
-                 legend.background=element_blank(),legend.title=element_blank())+
-          guides(colour = guide_legend(override.aes = list(size = 5)))
-p2
-
-
-
-library(ggplot2)
-library(ggthemes)
-
-mymap <- read.table("经纬度.txt", sep = "\t", header =T)
-world <- map_data("world")
-
 my_fill = c("Africa"="#984EA3","India_Paksitan"="#F781BF","South_China"="#E41A1C",
             "Central_South_Europe"="#FFFF33","Northeast_Asia"="#FF7F00",
             "Northwest_China"="#98F5FF","Tibet"="#377EB8","West_Europe"="#4DAF4A",
