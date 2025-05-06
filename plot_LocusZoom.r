d26301202d2667e1de61fbca36b19c9ae359e327
Loren Shi <crazzy_rabbit@163.com>
2025-05-06 08:58:04 +0800

    Update plot_LocusZoom.r

--- plot_LocusZoom.r
+++ plot_LocusZoom.r
@@ -1,7 +1,7 @@
 # ===========================================================================================//
 # @Author  :    Loren Shi
 # @Time    :    2025/04/27 10:37:43
-# @File    :    plot_Locouszoom.r
+# @File    :    plot_Locouszoom.R
 # @Mails   :    crazzy_rabbit@163.com
 # @line    :    https://github.com/Crazzy-Rabbit
 #
@@ -58,32 +58,32 @@ ReadLocusZoomData <- function(gwas, ld_info, snp, flank=500000){
 }
 
 plot_locuszoom <- function(data, genelist, flank=500000) {
-    top_snp=data$topsnp
-    region_data=datapltX$region_dt 
-    pltX=region_data$POS/1e6; 
-    pltY=-log10(region_data$p); 
-    pltcol=region_data$color 
+    top_snp=data$topsnp;
+    region_data=data$region_dt; 
+    gwasBP=region_data$POS/1e6; 
+    pXY=-log10(region_data$p); 
+    color=region_data$color; 
 
-    yMAX=ceiling(max(pltY))
-    offset_gene=yMAX/4
-    dev_axis=1.5
-    xmin=min(pltX,na.rm=T)-0.01; 
-    xmax=max(pltX,na.rm=T)+0.01; 
+    yMAX=ceiling(max(pXY));
+    offset_gene=yMAX/4;
+    dev_axis=1.5;
+    xmin=min(gwasBP,na.rm=T)-0.01; 
+    xmax=max(gwasBP,na.rm=T)+0.01; 
     ymin=-offset_gene-dev_axis; 
-    ymax=yMAX
+    ymax=yMAX;
 
     xlab=paste("Chromosome ", top_snp$CHR, " (Mb)")
     ylab=expression(-log[10] (italic(P)))
     
     #---------- plot start ----//
     par(mar=c(5,5,3,2), xpd=TRUE)
-    plot(pltX,pltY,pch=21,bg=pltcol,col="gray30",cex=1.8,lwd=0.2,yaxt="n",bty="n",
+    plot(gwasBP,pXY,pch=21,bg=color,col="gray30",cex=1.8,lwd=0.2,yaxt="n",bty="n",
     xlab=xlab, ylab="", xlim=c(xmin,xmax), ylim=c(ymin,ymax))
-    x_ticks=round(seq(xmin,xmax,length.out=6),2)
-    axis(1,at=x_ticks,lwd=1)
+    xticks=round(seq(xmin,xmax,length.out=6),2)
+    axis(1,at=xticks,lwd=1) 
     axis(2,at=seq(0,yMAX,by=yMAX/4),labels=round(seq(0,yMAX,yMAX/4),0),lwd=1,las=1)
     mtext(ylab, side=2, line=3, at=(yMAX*1/2))
-    segments(x0=min(pltX), y0=-0.5, x1=max(pltX), y1=-0.5, col="gray50", lwd=1, lty=2) 
+    segments(x0=min(gwasBP), y0=-0.5, x1=max(gwasBP), y1=-0.5, col="gray50", lwd=1, lty=2) 
 
     # top SNP
     points(top_snp$POS/1e6, -log10(top_snp$p), pch=23, bg="gold", cex=2, lwd=0.5)
@@ -91,12 +91,13 @@ plot_locuszoom <- function(data, genelist, flank=500000) {
 
     #-------- plot gene ----//
     glist=fread(genelist, col.names=c("CHR", "start", "end", "GENE", "strand"))
-    gene_chr=top_snp$CHR
-    gene_start=top_snp$POS-flank; 
-    gene_end=top_snp$POS+flank
-    generow=glist[CHR == gene_chr & start >= gene_start & end <= gene_end, ]
+    xcenter=top_snp$POS;
+    xchr=top_snp$CHR;
+    xstart=xcenter - flank; 
+    xend=xcenter + flank;
+    generow=glist[CHR == xchr & start >= xstart & end <= xend, ]
     generow$start=generow$start/1e6; 
-    generow$end=generow$end/1e6
+    generow$end=generow$end/1e6;
 
     # plot gene layer
     generow[, layer := 1]; 
@@ -114,44 +115,44 @@ plot_locuszoom <- function(data, genelist, flank=500000) {
             generow$layer[i]=current_layer
         }
     }
-    required_rows=max(generow$layer)
+    numrows=max(generow$layer)
     axis.start=ymin/2-1
     total_lay=abs(offset_gene+dev_axis)/5
-    if (required_rows %% 2==1){
-        median_layer=(required_rows+1)/2
-        generow[, y_pos := axis.start+(layer-median_layer)*0.8]
+    if (numrows %% 2==1){
+        median_layer=(numrows+1)/2
+        generow[, ypos := axis.start+(layer-median_layer)*0.8]
     } else {
-        median_upper=required_rows/2
-        generow[, y_pos := axis.start+(layer-median_upper-0.5)*0.8*total_lay]
+        median_upper=numrows/2
+        generow[, ypos := axis.start+(layer-median_upper-0.5)*0.8*total_lay]
     }
 
     for(i in 1:nrow(generow)) {
         arrow_code <- ifelse(generow$strand[i] == "+", 2, 1)
-        gene_len <- generow$end[i]-generow$start[i]
-        if (gene_len < 0.001) {
-            arrows(x0=generow$x_center[i]-0.0005, y0=generow$y_pos[i], x1=generow$x_center[i]+0.0005, y1=generow$y_pos[i], length=0.07, code=arrow_code, col="darkgoldenrod", lwd=1.5)} 
+        glen <- generow$end[i]-generow$start[i]
+        if (glen < 0.001) {
+            arrows(x0=generow$x_center[i]-0.0005, y0=generow$ypos[i], x1=generow$x_center[i]+0.0005, y1=generow$ypos[i], length=0.07, code=arrow_code, col="darkgoldenrod", lwd=1.5)} 
         else {
-            arrows(x0=generow$start[i], y0=generow$y_pos[i], x1=generow$end[i], y1=generow$y_pos[i], length=0.07, code=arrow_code, col="darkgoldenrod", lwd=1.5)}
-        text(x=generow$x_center[i], y=generow$y_pos[i], substitute(italic(gene), list(gene=generow$GENE[i])), pos=3, offset=0.3, cex=0.8, col="black")
+            arrows(x0=generow$start[i], y0=generow$ypos[i], x1=generow$end[i], y1=generow$ypos[i], length=0.07, code=arrow_code, col="darkgoldenrod", lwd=1.5)}
+        text(x=generow$x_center[i], y=generow$ypos[i], substitute(italic(gene), list(gene=generow$GENE[i])), pos=3, offset=0.3, cex=0.8, col="black")
     }
     
     #----------------- plot legend ----//
-    usr=par("usr")                    
-    legend_width=0.02*(usr[2]-usr[1])  # legend width set as 5% of fig region
-    left_margin=0.10*(usr[2]-usr[1]) 
-    down_margin=0.10*(usr[4]-usr[3])
-    breaks=c(0,0.2,0.4,0.6,0.8,1.0)
+    usr=par("usr");                    
+    legend_width=0.02*(usr[2]-usr[1]);  # legend width set as 5% of fig region
+    left_margin=0.10*(usr[2]-usr[1]);
+    down_margin=0.10*(usr[4]-usr[3]);
+    breaks=c(0,0.2,0.4,0.6,0.8,1.0);
 
-    legend_x_left=usr[2]-legend_width-left_margin 
-    legend_x_right=usr[2]-left_margin
-    y_bottom=usr[4]-0.3*(usr[4]-usr[3])-down_margin  
-    y_top=usr[4]-down_margin                           
-    y_pos=seq(y_bottom, y_top, length.out=length(breaks)) 
+    legend_x_left=usr[2]-legend_width-left_margin; 
+    legend_x_right=usr[2]-left_margin;
+    legend_y_bottom=usr[4]-0.3*(usr[4]-usr[3])-down_margin;  
+    legend_y_top=usr[4]-down_margin;                           
+    legend_y_pos=seq(legend_y_bottom, y_top, length.out=length(breaks)); 
     
     tit=expression(italic(r)^2)
     for (i in 1:(length(breaks)-1)) {
-        rect(legend_x_left, y_pos[i], legend_x_right, y_pos[i+1], col=color_pal[i], border=NA)
+        rect(legend_x_left, legend_y_pos[i], legend_x_right, legend_y_pos[i+1], col=color_pal[i], border=NA)
     }
-    text(x=legend_x_left, y=y_pos, labels=breaks, pos=2, cex=0.7, xpd=TRUE)
-    text(x=legend_x_left, y=y_top, labels=tit, pos=3, cex=0.9)
+    text(x=legend_x_left, y=legend_y_pos, labels=breaks, pos=2, cex=0.7, xpd=TRUE)
+    text(x=legend_x_left, y=legend_y_top, labels=tit, pos=3, cex=0.9)
 }
